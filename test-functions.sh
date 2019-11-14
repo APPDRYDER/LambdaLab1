878aa8ea62df9588eb147c357c1ae1ebfcf831a0
David <davidryder@yahoo.com>
2019-11-14 14:43:58 -0800

    add _startJavaApp

--- test-functions.sh
+++ test-functions.sh
@@ -44,3 +44,47 @@ _testJavaAppLoadGen1() {
   done
   echo "Complete _testJavaAppLoadGen1"
 }
+
+_startJavaApp() {
+  NODE_ID="1"
+
+  _validateEnvironmentVars "Run Java App with AppDynmaics Java Agent" \
+    "APPDYNAMICS_APPLICATION_AGENT_JAR_FILE" "APPDYNAMICS_APPLICATION_NAME"
+
+  # Class path and options
+  export JAVA_OPTS=""
+  if [ -e "$APPDYNAMICS_APPLICATION_AGENT_JAR_FILE" ]; then
+    export JAVA_OPTS="$JAVA_OPTS -javaagent:$APPDYNAMICS_APPLICATION_AGENT_JAR_FILE "
+  else
+    echo "AppDynamics Java Agent not found (APPDYNAMICS_APP_AGENT_JAR_FILE): $APPDYNAMICS_APPLICATION_AGENT_JAR_FILE"
+    exit 0
+  fi
+  export JAVA_OPTS=$JAVA_OPTS"-Djava.security.egd=file:/dev/./urandom "
+  export JAVA_OPTS=$JAVA_OPTS"-Dappdynamics.low.entropy=true "
+  export JAVA_OPTS=$JAVA_OPTS"-Dallow.unsigned.sdk.extension.jars=true "
+  export JAVA_OPTS=$JAVA_OPTS"-Dappdynamics.analytics.agent.url=$APPDYNAMICS_ANALYTICS_AGENT_URL "
+
+  clear
+  echo "JAVA_OPS [$JAVA_OPTS]"
+  rm -rf nohup.out
+  touch nohup.out
+
+  # Stop
+  echo "Stop running instance of $JAVA_TEST_APP_JAR"
+  ps -ef | grep "$JAVA_TEST_APP_JAR" ; sleep 1
+  pkill -f "$JAVA_TEST_APP_JAR" ; sleep 1
+
+
+  PARAM_NODE_PORT=$((18080 + NODE_ID)) #18081, 18082, .....
+  unset APPDYNAMICS_AGENT_TIER_NAME
+  unset APPDYNAMICS_AGENT_NODE_NAME
+
+  APPD_OPTIONS=""
+  APPD_OPTIONS=$APPD_OPTIONS"-Dappdynamics.agent.applicationName=$APPDYNAMICS_APPLICATION_NAME "
+  APPD_OPTIONS=$APPD_OPTIONS"-Dappdynamics.agent.tierName=JAVA_TIER_T1 "
+  APPD_OPTIONS=$APPD_OPTIONS"-Dappdynamics.agent.nodeName=JAVA_NODE_N$NODE_ID"
+  echo "Starting: Node: $NODE_ID on Port: $PARAM_NODE_PORT AppName: $APPDYNAMICS_APPLICATION_NAME"
+  nohup java $JAVA_OPTS $APPD_OPTIONS -jar $JAVA_TEST_APP_JAR $PARAM_NODE_PORT &
+
+  _tailLog 300 nohup.out
+}

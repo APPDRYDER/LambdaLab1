218e51cffa2175466e1a94203e52dfef66cf5564
David <davidryder@yahoo.com>
2019-11-16 16:20:39 -0800

    dockerBuild

--- ctl.sh
+++ ctl.sh
@@ -129,6 +129,15 @@ elif [ $cmd == "buildJavaApp" ]; then
   $MVN_BIN -f JavaApp/pom.xml assembly:single
   # Built LambdaFunction/target/LambdaFunction-0.0.1-SNAPSHOT.jar
 
+elif [ $cmd == "dockerBuild" ]; then
+  # Copy in AppDynamcis agents
+  cp ~/Downloads/AppD-Downloads/AppServerAgent-4.5.15.28231.zip .
+  cp ~/Downloads/AppD-Downloads/MachineAgent-4.5.14.2293.zip .
+  cp ~/Downloads/AppD-Downloads/awslambdamonitor-2.0.1.zip .
+  ls -al *.zip
+  #
+  docker build -t lambdalab1 .
+
 elif [ $cmd == "test1" ]; then
   _test1 A B C
 

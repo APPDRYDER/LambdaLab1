4eb71d229482d26a78c70543b6709cbdbda0ec7d
David <davidryder@yahoo.com>
2019-11-23 16:52:45 -0800

    JSON in POST

--- JavaApp/src/pkg1/AwsHandler1.java
+++ JavaApp/src/pkg1/AwsHandler1.java
@@ -2,6 +2,7 @@ package pkg1;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -14,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.json.JSONObject;
 
 public class AwsHandler1 extends AbstractHandler {
 	private int totalReads = 0;
@@ -22,7 +24,6 @@ public class AwsHandler1 extends AbstractHandler {
 	private Random r1 = new Random();
 	AwsLambda al = new AwsLambda();
 
-	
 	public AwsHandler1() {
 		ut = new pkg1.Utils();
 	} // Test1Handler
@@ -31,20 +32,33 @@ public class AwsHandler1 extends AbstractHandler {
 		// log1.info(s);
 		System.out.println(s);
 	}
-	
+
 	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		String body = null;
+		String bodyStr = null;
 		PrintWriter out = response.getWriter();
 
 		System.out.println(String.format("[%s, %s, %s]", baseRequest.getMethod(), baseRequest.getRequestURL(),
 				baseRequest.getPathInfo()));
-		
+
 		if (baseRequest.getMethod() == "POST") {
-			body = request.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual);
-			int waitRate = ut.getIntParameter(request, "waitRate", 0);
-			int errorRate = ut.getIntParameter(request, "errorRate", 0);
-			System.out.println(String.format("POST w %d e %d", waitRate, errorRate));
+			bodyStr = request.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual);
+			System.out.println(String.format("POST body %s", bodyStr));
+			try {
+				JSONObject j = new JSONObject(bodyStr);
+				String awsRegion = ut.getJSONStringParameter(j, "region", "");
+				String awsApiId = ut.getJSONStringParameter(j, "apiId", "");
+				String awsStage = ut.getJSONStringParameter(j, "stage", "");
+				String apiPath = ut.getJSONStringParameter(j, "apiPath", "");
+				String apiKey = ut.getJSONStringParameter(j, "apiKey", "");
+				String message = ut.getJSONStringParameter(j, "message", "");
+				System.out.println(String.format("POST(%s, %s, %s, %s, %s, %s)", awsRegion, awsApiId, awsStage, apiPath,
+						apiKey, message));
+				System.out.println(String.format("JSON ok %s", bodyStr));
+				//j.put("duration", durationTime);
+			} catch (Exception e) {
+				System.out.println(String.format("JSON error %s, %s", bodyStr, e));
+			}
 			response.setContentType("text/html; charset=utf-8");
 			response.setStatus(HttpServletResponse.SC_OK);
 
@@ -52,12 +66,12 @@ public class AwsHandler1 extends AbstractHandler {
 			// Expects /thread/fn
 			// curl -G 0.0.0.0:18081/threads/ --data-urlencode "threads=1" --data-urlencode
 			// "reads=4"
-			//ut.printParameterMap(request);
+			// ut.printParameterMap(request);
 			response.setContentType("text/html; charset=utf-8");
 			if (baseRequest.getPathInfo().equals("/health")) {
 				out.println(String.format("ok"));
 				System.out.println("ok");
-				
+
 			} else if (baseRequest.getPathInfo().equals("/API")) {
 				String awsRegion = ut.getStringParameter(request, "region", "");
 				String awsApiId = ut.getStringParameter(request, "apiId", "");
@@ -65,9 +79,10 @@ public class AwsHandler1 extends AbstractHandler {
 				String apiPath = ut.getStringParameter(request, "apiPath", "");
 				String apiKey = ut.getStringParameter(request, "apiKey", "");
 				String message = ut.getStringParameter(request, "message", "");
-				System.out.println(String.format("(%s, %s, %s, %s, %s, %s)", awsRegion, awsApiId, awsStage, apiPath, apiKey, message));
+				System.out.println(String.format("(%s, %s, %s, %s, %s, %s)", awsRegion, awsApiId, awsStage, apiPath,
+						apiKey, message));
 				al.sendApiCall(awsRegion, awsApiId, awsStage, apiPath, apiKey, message);
-				
+
 			} else {
 				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 				out.println(String.format("<h1>PathInfo Not found [%s, (%s)]</h1>", baseRequest.getRequestURL(),
@@ -79,9 +94,9 @@ public class AwsHandler1 extends AbstractHandler {
 			response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
 		}
 
-		if (body != null) {
-			out.println(body);
-		}
+		//if (body != null) {
+		//	out.println(body);
+		//}
 		baseRequest.setHandled(true);
 	} // handle
 } // Test1Handler
